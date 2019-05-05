55c834a090f200998b4545442be6f1589e72370e
dexterpuru <pratyaksh2000saini@gmail.com>
2019-05-06 04:26:52 +0530

    Added Inter-process communication

---
+++ pipes/pipe_reader.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+int main() {
+	FILE *rd;
+	char buffer[50];
+
+	rd=popen("ls","r");
+	fread(buffer, 1, 50, rd);
+
+	printf("%s\n", buffer);
+	pclose(rd);
+}
+
